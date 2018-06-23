cf72ab4e1df09e8ae39e30f069b11a4db6442486
Gaëtan Rizio <gaetan@rizio.fr>
2018-06-23 20:54:38 +0200

    Accept generics when validating an expression
    When using generics, the compiler set a generator as invalid
    because it returned a generic type, defined itself in the function signature.
    
    This commit correct the behavior so generic types are accepted in expressions.
    
    This commit does the following:
    
    * Add generics in ScopedExpression
    * If an expression returns a generic type, accept it in control
    * Add test to verify the behavior

--- src/main/scala/definiti/tests/validation/controls/expression/ValidExpressionTypeControl.scala
+++ src/main/scala/definiti/tests/validation/controls/expression/ValidExpressionTypeControl.scala
@@ -19,19 +19,21 @@ object ValidExpressionTypeControl extends Control[ValidationContext] {
 
   private def controlExpression(expression: ScopedExpression[Expression], context: ValidationContext): ControlResult = {
     val typ = expression.typeOfExpression
-    if (isTypeValid(typ, context)) {
+    if (isTypeValid(typ, context, expression.definedGenerics)) {
       ControlResult.OK
     } else {
       invalidType(typ, expression.location)
     }
   }
 
-  private def isTypeValid(typ: Type, context: ValidationContext): Boolean = {
-    context.getClassDefinition(typ.name)
+  private def isTypeValid(typ: Type, context: ValidationContext, generics: Seq[String]): Boolean = {
+    val isGeneric = generics.contains(typ.name)
+    val isValidClass = context.getClassDefinition(typ.name)
       .exists { classDefinition =>
         typ.generics.length == classDefinition.genericTypes.length &&
-          typ.generics.forall(isTypeValid(_, context))
+          typ.generics.forall(isTypeValid(_, context, generics))
       }
+    isGeneric || isValidClass
   }
 
   def invalidType(typ: Type, location: Location): Alert = {
